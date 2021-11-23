c4bbcc2d510866276de522f2eb68f19235fdfb30
noble-student <78581571+noble-student@users.noreply.github.com>
2021-11-22 23:35:06 -0800

    Create MemberType.cpp
    initial commit

---
+++ MemberType.cpp
@@ -0,0 +1,40 @@
+#include "MemberType.h"
+#include <iostream>
+void MemberType::setMemberInfo(const std::string& newFirst, 
+							   const std::string& newLast, int newID)
+{
+	firstName = newFirst;
+	lastName = newLast;
+	memberID = newID;
+}
+const std::string& MemberType::getFirstName() const
+{
+	return firstName;
+}
+
+const std::string& MemberType::getLastName() const
+{
+	return lastName;
+}
+
+const int& MemberType::getMembershipNo() const
+{
+	return memberID;
+}
+
+void MemberType::printName() const
+{
+	std::cout << firstName << " " 
+			<< lastName << std::endl;
+}
+
+void MemberType::printMemberInfo() const
+{
+	std::cout << memberID << " - " << firstName 
+		<< " " << lastName << std::endl;
+}
+
+MemberType::~MemberType()
+{
+	// UNSURE IF ANYTHING NEEDS TO BE DELETED.
+}
