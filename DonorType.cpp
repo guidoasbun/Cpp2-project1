2c3d9e9b2a45bec5c3a02a86d79260ced20833b1
GuidoAsbun <guido@asbun.io>
2021-11-23 00:15:56 -0800

    Created DonorType.cpp

---
+++ DonorType.cpp
@@ -0,0 +1,24 @@
+
+#include "DonorType.h"
+#include <iostream>
+
+void setDonorInfo(const std::string&, const std::string&, int, double)
+{
+
+}
+
+double setAmountDonated(double& amount)
+{
+    return 0.0;
+}
+
+void printDonor()
+{
+
+}
+
+
+void printDonation()
+{
+
+}
