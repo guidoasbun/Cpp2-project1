42d4bb8437f84e2094f6ca0a3e47405a0c4fa5c2
Harry Derderian <82200862+HarryDerderian@users.noreply.github.com>
2021-11-25 18:05:29 -0800

    Update Database.h

--- Database.h
+++ Database.h
@@ -1,23 +1,18 @@
-#ifndef DATABASE_H
-#define DATABASE_H_H
-#include <set>
-#include "DonorType.h"
-
-/* If the set is returned by ref the program will crash
-*  only return the set by value, unless you can find a way to store it in the heap.
-* 
+/*
+	Noble Team
+	Noble, Trevor
+	Derderian, Harry
+	Kaboli, Seena
+	Asbun, Guido
+	Kim, Yeomyung
+	November 22, 2021
+	CS A250
+	Project 1
 */
-const std::set<DonorType*> getData()
-{
-	/* use new operator to store the donors in the heap
-	* so their life time outlives the function.
-	* important note make sure to create a set based on the set returned here.
-	* if we don't do that we will have memory leaks within the heap 
-	* (we will lose the pointers to the donors)
-	*/
-	DonorType* aDonor2 = new DonorType("Harry", "Derderian", 20, 750.25);
-	std::set<DonorType*> donors;
-	donors.insert(aDonor2);
-	return donors;
-}
+#ifndef DATABASE_H
+#define DATABASE_H
+#include "DonorType.h"
+#include <set>
+#include <string>
+std::set<DonorType> getData();
 #endif
