d6c63551ff730e67721585756943e0c045416a71
GuidoAsbun <guido@asbun.io>
2021-11-27 10:23:37 -0800

    Started DonorList.cpp

---
+++ DonorList.cpp
@@ -0,0 +1,63 @@
+/*
+	Noble Team
+
+	Noble, Trevor
+	Derderian, Harry
+	Kaboli, Seena
+	Asbun, Guido
+	Kim, Yeomyung
+
+	November 22, 2021
+
+	CS A250
+	Project 1
+*/
+
+#include "DonorList.h"
+
+using namespace std;
+
+void DonorList::addDonor(const std::string &firstName, const std::string &lastName, int memberNum, int donation)
+{
+
+}
+
+void DonorList::createList()
+{
+
+}
+
+int DonorList::getNoOfDonors() const
+{
+
+}
+
+double DonorList::getTotalDonations() const
+{
+
+}
+
+bool DonorList::isEmpty() const
+{
+
+}
+
+bool DonorList::searchID(int memberID) const
+{
+
+}
+
+void DonorList::deleteDonor(int memberID)
+{
+
+}
+
+void DonorList::printAllDonors() const
+{
+
+}
+
+void DonorList::clearList()
+{
+
+}
