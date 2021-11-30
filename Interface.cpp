dd33eda1a761a7975822ae30ff93b2b935e1b2dc
GuidoAsbun <guido@asbun.io>
2021-11-30 13:20:14 -0800

    Created Interface.cpp/h they are blank

---
+++ Interface.cpp
@@ -0,0 +1,16 @@
+/*
+	Noble Team
+
+	Noble, Trevor
+	Derderian, Harry
+	Kaboli, Seena
+	Asbun, Guido
+	Kim, Yeomyung
+
+	November 22, 2021
+
+	CS A250
+	Project 1
+*/
+
+#include "Interface.h"
---
+++ Interface.h
@@ -0,0 +1,19 @@
+/*
+	Noble Team
+
+	Noble, Trevor
+	Derderian, Harry
+	Kaboli, Seena
+	Asbun, Guido
+	Kim, Yeomyung
+
+	November 22, 2021
+
+	CS A250
+	Project 1
+*/
+
+#ifndef INTERFACE_H
+#define INTERFACE_H
+
+#endif //CPP2_PROJECT1_INTERFACE_H
