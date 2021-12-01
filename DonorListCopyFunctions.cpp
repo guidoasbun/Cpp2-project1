be40c7ce3f1b84f846679bc31a5c3fd9f3ddf93f
Harry Derderian <82200862+HarryDerderian@users.noreply.github.com>
2021-12-01 06:45:50 -0800

    Copy constructor 1.0

--- DonorListCopyFunctions.cpp
+++ DonorListCopyFunctions.cpp
@@ -1,1 +1,24 @@
+#include "DonorList.h"
+#include "DonorType.h"
+#include "Database.h"
+#include "set"
 
+using namespace std;
+
+DonorList::DonorList(const DonorList& otherList)
+{
+	first = nullptr;
+	last = first;
+	count = 0;
+	Node* current = otherList.first;
+	while (current != nullptr)
+	{
+		DonorType donor = current->getDonor();
+		addDonor(donor.getFirstName(), donor.getLastName(), 
+				 donor.getMembershipNo(), donor.getAmountDonated() );
+		current = current->getPtrToNext();
+	}
+	/*
+	Using the function addDonor, copy each donor from the parameter list into the calling list.
+	*/
+}
